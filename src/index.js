1ced3d5ec5808fa0ac430b391801c5977f5830c4
panteslav <panteslav@yandex.ru>
2020-02-26 22:16:23 +0300

    exclamation mark added

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import readlineSync from 'readline-sync';
 const getUserName = () => {
     const userName = readlineSync.question('May I have your name? ');
     
-    console.log(`Hello, ${userName}`);
+    console.log(`Hello, ${userName}!`);
 };
 
 export default getUserName;
