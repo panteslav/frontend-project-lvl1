c3e93e9151c0d3ec68d6f3fcb1df1737f2e64331
panteslav <panteslav@yandex.ru>
2020-02-26 22:20:58 +0300

    extra commentaries removed

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
 import readlineSync from 'readline-sync';
-//const greeting = () => {
-    
-//    console.log(`Hello, ${userName}!`);
-//};
 
 const getUserName = () => {
     const userName = readlineSync.question('May I have your name? ');
