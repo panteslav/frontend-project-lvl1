d07b9017888ed5b1b418ed4643b17bc06de10656
panteslav <panteslav@yandex.ru>
2020-02-29 16:40:01 +0300

    gitHub actions

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,11 +1,7 @@
 #!/usr/bin/env node
 
-import getUserName from "/home/panteslav/Desktop/brainGames/frontend-project-lvl1/src/index.js";
+import getUserName from '../src/index.js';
 
-console.log("Welcome to the Brain Games!");
+console.log('Welcome to the Brain Games!');
 
 getUserName();
-
-
-
-
