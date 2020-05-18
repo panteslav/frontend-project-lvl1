90ef02a2a4e2e44f6640a8f7a2a84d206fec19a3
panteslav <panteslav@yandex.ru>
2020-05-18 19:35:00 +0300

    brain-progression game optimisation

--- src/Games/brain-progression.js
+++ src/Games/brain-progression.js
@@ -3,33 +3,25 @@ import playGame from '../index.js';
 
 const PROGRESSION_LENGTH = 10;
 
-const generateProgression = () => {
+const generateProgression = (start, ratio, length) => {
   const progression = [];
-  const firstNum = _.random(0, 500);
-  const ratio = _.random(0, 500);
-
-  for (let i = 0, n = firstNum; i < PROGRESSION_LENGTH; i += 1, n += ratio) {
+  for (let i = 0, n = start; i < length; i += 1, n += ratio) {
     progression.push(n);
   }
+
   return progression;
-};
-
-const handleProgression = (progression) => {
-  const numbers = progression;
-  const randomPosition = _.random(0, numbers.length - 1);
-  const elementToRemove = numbers[randomPosition];
-  numbers[randomPosition] = '..';
-
-  return [numbers, elementToRemove];
 };
 
 const instructionText = 'What number is missing in the progression?';
 
 const getQnA = () => {
-  const baseProgression = generateProgression();
-  let [question, answer] = handleProgression(baseProgression);
-  question = question.join(' ');
-  answer = answer.toString();
+  const firstNum = _.random(0, 500);
+  const ratio = _.random(0, 500);
+  const progression = generateProgression(firstNum, ratio, PROGRESSION_LENGTH);
+
+  const randomPosition = _.random(0, progression.length - 1);
+  const answer = progression.splice(randomPosition, 1, '..').toString();
+  const question = progression.join(' ');
 
   return [question, answer];
 };
